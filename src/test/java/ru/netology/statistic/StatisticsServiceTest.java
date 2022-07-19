f3ca0301a056215c234191d89422c8a1142d2430
Маргарита Мусатова <musatovamargarita@gmail.com>
2022-07-19 14:57:45 +0300

    100 % покрытие

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMax1() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {13, 5, 8, 4, 5, 3, 8, 6, 11, 11, 14};
+        long expected = 14;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
